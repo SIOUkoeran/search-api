9ed76fc4e3b7b00debc4a8f76c355681771503ae
SIOUkoeran <alstnalsgud@gmail.com>
2022-09-10 19:26:49 +0900

    #1 Fix logstash config
    set_up
    
    - logstash 시작 전, 별도 컨테이너를 통해 index 생성, 설정
    
    logstash
    
    - poi, address 인덱스 별 csv 파일 저장
    - sincedb_path 폴더 권한 설정 변경

--- elasticsearch/config/es_schema.sh
+++ elasticsearch/config/es_schema.sh
@@ -2,35 +2,90 @@
 
 curl -XPUT http://cluster1-master-node:9200/address -H Content-Type:application/json -d '
 {
+	"settings" : {
+		"index" : {
+			"number_of_shards" : 3,
+			"number_of_replicas" : 1
+		},
+		"analysis" : {
+			"tokenizer" : {
+				"nori" : {
+					"type" : "nori_tokenizer",
+					"decompound_mode" : "mixed"
+				}
+			},
+			"analyzer" : {
+				"nori" : {
+					"type" : "custom",
+					"tokenizer" : "nori_tokenizer",
+					"filter" : ["nori_readingform", "nori_number"]
+				}
+			}
+		},
+		"refresh_interval" : "30s"
+	},
 	"mappings" : {
 		"properties" : {
 			"poi_code" : {"type" : "keyword"},
-			"fname" : {"type" : "text"},
-			"cname" : {"type" : "text"},
+			"fname" : {
+				"type" : "text",
+				"analyzer" : "nori"
+				},
+			"cname" : {
+				"type" : "text",
+				"analyzer" : "nori"
+				},
 			"phone_a" : {"type" : "integer"},
 			"phone_b" : {"type" : "integer"},
 			"phone_c" : {"type" : "integer"},
 			"zip_code" : {"type" : "integer"},
-			"address" : {"type" : "text"},
+			"address" : {
+				"type" : "text",
+				"analyzer" : "nori"
+				},
 			"san_bun" : {"type" : "integer"},
 			"primary_bun" : {"type" : "integer"},
-			"secondary" : {"type" : "integer"},
-			"logtitute" : {"type" : "geo_point"},
-			"latitude" : {"type" : "geo_point"}
+			"secondary_bun" : {"type" : "integer"},
+			"location" : {
+				"type" : "geo_point",
+				"field" : {
+					"lon" : {"type" : "half_float"},
+					"lat" : {"type" : "half_float"}
+				}
+			}
 		}
 	}
 }'
 
-sleep 2
-
 curl -XPUT http://cluster1-master-node:9200/poi -H Content-Type:application/json -d '
 {
+	"settings" : {
+		"index" : {
+			"number_of_shards" : 3,
+			"number_of_replicas" : 1
+		},
+		"analysis" : {
+			"tokenizer" : {
+				"nori" : {
+					"type" : "nori_tokenizer",
+					"decompound_mode" : "mixed"
+				}
+			},
+			"analyzer" : {
+				"nori" : {
+					"type" : "custom",
+					"tokenizer" : "nori_tokenizer"
+				}
+			}
+		},
+		"refresh_interval" : "30s"
+	},
 	"mappings" : {
 		"properties" : {
 			"poi_code" : {"type" : "keyword"},
 			"large_category" : {"type" : "keyword"},
-			"medium_category" : {"type" : "keyword"},
-			"small_category" : {"type" : "keyword"}
+			"medium_cateogry" : {"type" : "keyword"},
+			"small_category" : {"type" : "keyword}
 		}
 	}
 }'
