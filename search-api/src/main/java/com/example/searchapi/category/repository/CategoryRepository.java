3d04b475e010525aa6c78ee7629177344b5bead4
SIOUkoeran <alstnalsgud@gmail.com>
2022-09-28 23:47:34 +0900

    Revert "Feat/search address (#7)"
    This reverts commit 7a7620e5b997c8167f9e926d9cd0b76f93389429.

--- search-api/src/main/java/com/example/searchapi/category/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.searchapi.category.repository;
-
-import com.example.searchapi.category.model.Category;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends ElasticsearchRepository<Category, String> {
-}
--- search-api/src/test/java/com/example/searchapi/BaseTest.java
+++
@@ -1,8 +0,0 @@
-package com.example.searchapi;
-
-import com.example.searchapi.container.TestContainer;
-import org.springframework.context.annotation.Import;
-
-@Import({TestContainer.class})
-public class BaseTest {
-}
