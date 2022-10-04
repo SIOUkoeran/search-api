13beb5a9343ac80f7f57e3ba8785f07c00bb5fa6
SIOUkoeran <alstnalsgud@gmail.com>
2022-10-04 18:43:30 +0900

    #10 ADD poi 자동완성 기능 추가
    poi 이름 자동완성 검색 기능 추가

---
+++ search-api/src/main/java/com/example/searchapi/poi/api/PoiSuggestController.java
@@ -0,0 +1,31 @@
+package com.example.searchapi.poi.api;
+
+
+import com.example.searchapi.poi.dto.PoiSuggestDto;
+import com.example.searchapi.poi.service.PoiService;
+import com.example.searchapi.poi.service.PoiSuggestService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/poi/suggest")
+@Slf4j
+public class PoiSuggestController {
+
+    private final PoiService poiService;
+    private final PoiSuggestService poiSuggestService;
+
+    public PoiSuggestController(PoiService poiService, PoiSuggestService poiSuggestService) {
+        this.poiService = poiService;
+        this.poiSuggestService = poiSuggestService;
+    }
+
+    @GetMapping(value = "")
+    public ResponseEntity<PoiSuggestDto.Response> suggestPoiName(PoiSuggestDto.Request request) {
+        log.info("request suggest {}", request.getPoi());
+        return ResponseEntity.ok(this.poiSuggestService.suggestPoi(request));
+    }
+}
