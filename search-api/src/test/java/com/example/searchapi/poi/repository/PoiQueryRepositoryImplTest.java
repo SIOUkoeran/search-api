385a5f9b86e634cffa4fc29c696182381aadb79a
김민수 <alstnalsgud@gmail.com>
2022-10-07 18:46:03 +0900

    Feat/typo correction (#16)
    오타보정 기능 추가, 테스트 코드 추가

--- search-api/src/test/java/com/example/searchapi/poi/repository/PoiQueryRepositoryImplTest.java
+++ search-api/src/test/java/com/example/searchapi/poi/repository/PoiQueryRepositoryImplTest.java
@@ -1,17 +1,38 @@
 package com.example.searchapi.poi.repository;
 
+import com.example.searchapi.BaseTest;
+import com.example.searchapi.poi.model.Poi;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-class PoiQueryRepositoryImplTest {
+
+@SpringBootTest
+class PoiQueryRepositoryImplTest extends BaseTest {
+
+
+    @Autowired
+    PoiQueryRepository poiQueryRepository;
+
+    final Logger log = LoggerFactory.getLogger(PoiQueryRepositoryImplTest.class);
 
     @Test
-    void reverseString() {
-        String s = "abcd";
-        for (int i = s.length() - 1; i >= 0; i--){
-            System.out.println(s.charAt(i));
-        }
+    @DisplayName("오타보정 테스트 fuzziness 2 일 때")
+    void testLevenshteinDistanceInAUTO() {
+        String name = "오시오장";
+        List<Poi> pois = this.poiQueryRepository.searchPoiByName(name, PageRequest.of(0, 10));
+        assertThat(pois.size()).isEqualTo(5);
+        pois
+                .forEach(poi -> log.info("poi : {}", poi.getFname()));
     }
 
 }
